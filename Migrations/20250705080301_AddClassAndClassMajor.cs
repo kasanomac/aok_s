ae11ae3072584384cb72dbacd456732fc4da41b0
kasanomac <mailaddress0422@icloud.com>
2025-07-06 11:19:18 +0900

    add Class table

---
+++ Migrations/20250705080301_AddClassAndClassMajor.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aok_s.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClassAndClassMajor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClassName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MajorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Class_Majors_MajorId",
+                        column: x => x.MajorId,
+                        principalTable: "Majors",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ClassMajor",
+                columns: table => new
+                {
+                    ClassId = table.Column<int>(type: "int", nullable: false),
+                    MajorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassMajor", x => new { x.ClassId, x.MajorId });
+                    table.ForeignKey(
+                        name: "FK_ClassMajor_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClassMajor_Majors_MajorId",
+                        column: x => x.MajorId,
+                        principalTable: "Majors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Class_MajorId",
+                table: "Class",
+                column: "MajorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClassMajor_MajorId",
+                table: "ClassMajor",
+                column: "MajorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassMajor");
+
+            migrationBuilder.DropTable(
+                name: "Class");
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,8 @@
+public class Class
+{
+    public int Id { get; set; }
+    public string ClassName { get; set; }
+
+    // Navigation property（中間テーブルへの）
+    public ICollection<ClassMajor> ClassMajors { get; set; }
+}
