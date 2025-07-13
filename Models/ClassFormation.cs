1d9e75328686d01cf53feb4e8d40b9ef463b9c52
kasanomac <mailaddress0422@icloud.com>
2025-07-13 13:52:45 +0900

    AddClassFormationBetweenSemesterAndDepartment

---
+++ Models/ClassFormation.cs
@@ -0,0 +1,10 @@
+public class ClassFormation
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public int SemesterId { get; set; }
+    public Semester Semester { get; set; } = default!;
+
+    public ICollection<Department> Departments { get; set; } = new List<Department>();
+}
--- Models/Semester.cs
+++ Models/Semester.cs
@@ -2,5 +2,6 @@ public class Semester
 {
     public int Id { get; set; } //SemesterId
     public string SemesterName { get; set; } = string.Empty; // 前期、後期
-    public ICollection<Department> Departments { get; set; } = new List<Department>();
+
+    public ICollection<ClassFormation> ClassFormations { get; set; } = new List<ClassFormation>();
 }
