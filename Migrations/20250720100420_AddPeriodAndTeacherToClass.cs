065e1d12a5956c7c602981f977deafc2fc09f345
kasanomac <mailaddress0422@icloud.com>
2025-07-20 20:10:58 +0900

    add Period and Teacher to Class

---
+++ Migrations/20250720100420_AddPeriodAndTeacherToClass.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aok_s.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPeriodAndTeacherToClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Period",
+                table: "Classes",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Teacher",
+                table: "Classes",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "Classes");
+
+            migrationBuilder.DropColumn(
+                name: "Teacher",
+                table: "Classes");
+        }
+    }
+}
